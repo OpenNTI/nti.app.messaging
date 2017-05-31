8ee4ffc56a352d998c54e96278b3f665ed94fcc5
Carlos Sanchez <papachoco@gmail.com>
2017-05-30 21:17:17 -0500

    add supporting files

--- README.rst
+++ README.rst
@@ -1,3 +1,9 @@
-==========
+===================
  nti.app.messaging
-==========
+===================
+
+.. image:: https://travis-ci.org/NextThought/nti.app.messaging.svg?branch=master
+    :target: https://travis-ci.org/NextThought/nti.app.messaging
+
+.. image:: https://coveralls.io/repos/github/NextThought/nti.app.messaging/badge.svg?branch=master
+    :target: https://coveralls.io/github/NextThought/nti.app.messaging?branch=master
--- setup.py
+++ setup.py
@@ -12,13 +12,14 @@ def _read(fname):
     with codecs.open(fname, encoding='utf-8') as f:
         return f.read()
 
+
 setup(
     name='nti.app.messaging',
     version=_read('version.txt').strip(),
     author='Jason Madden',
     author_email='jason@nextthought.com',
-    description="NTI app messaging",
-    long_description=_read('README.rst'),
+    description="NTI Application Messaging",
+    long_description=(_read('README.rst') + '\n\n' + _read('CHANGES.rst')),
     license='Apache',
     keywords='pyramid messaging',
     classifiers=[
@@ -27,6 +28,7 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: Implementation :: CPython',
     ],
     zip_safe=True,
     packages=find_packages('src'),
@@ -35,7 +37,7 @@ setup(
     namespace_packages=['nti', 'nti.app'],
     install_requires=[
         'setuptools',
-        'nti.messaging'
+        'nti.messaging',
     ],
     entry_points=entry_points,
 )
